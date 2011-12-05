4600810c830a396587e896be679f9b7ec1aef244
Tom Ashworth <tom@phuu.net>
2011-12-05 21:25:41 +0000

    Server debug

--- test.php
+++ test.php
@@ -3,22 +3,5 @@
 
   print_r($_SERVER);
   
-  /*
-  $config = new Config();
-  
-  $config->db = array(
-    "host" => "localhost"
-  , "user" => "root"
-  , "password" => "root"
-  , "db" => "lowcarb"
-  );
-  
-  $db = new DB($config->db);
-  
-  $uri = new URI();
-  
-  $articles = new Model("articles", $db);
-  */
-  
 ?>
 </pre>
