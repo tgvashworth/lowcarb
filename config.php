11df9f1a2356c87504e9db2d463e745a3e379558
Tom Ashworth <tom@phuu.net>
2012-04-29 17:29:58 +0200

    Update config.php

--- config.php
+++ config.php
@@ -3,36 +3,13 @@
   // Configure lowcarb
   
   /* Development config */
-  $config->url = "//blog.dev/";
-  $config->salt = "cardiffschoolofcomputerscience";
+  $config->url = "//something.dev/";
+  $config->salt = "somesalt";
   $config->db = array(
     "host" => "localhost"
   , "user" => "root"
   , "password" => "root"
-  , "db" => "lowcarb"
-  );
-  $config->routes = array(
-    "" => "index"
-  , "on" => "on"
-  , "write" => "write"
-  , "edit" => "edit"
-  , "logout" => "out"
-  , "error" => "error"
-  , "comment" => "comment"
-  , "delete" => "delete"
-  );
-
-  /* Production config */
-  $config->environment('production');
-  
-  $config->url = "//project.cs.cf.ac.uk/T.Ashworth/blog/";
-  $config->prefix = "/T.Ashworth/blog";
-  $config->salt = "cardiffschoolofcomputerscience";
-  $config->db = array(
-    "host" => "ephesus.cs.cf.ac.uk"
-  , "user" => "c1103808"
-  , "password" => "howbau"
-  , "db" => "c1103808"
+  , "db" => "something"
   );
   $config->routes = array(
     "" => "index"
